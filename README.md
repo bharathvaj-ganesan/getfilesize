95fc6886f7b43389da8425783e3f531960210231
bharath <bharathvaj1995@gmail.com>
2017-06-10 01:23:18 +0530

    Upgrade commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# getfilesize
+[![Built with Grunt](https://cdn.gruntjs.com/builtwith.svg)](https://gruntjs.com/)
+[![Build Status](https://travis-ci.org/bharathvaj1995/array-random-shuffle.svg?branch=master)](https://travis-ci.org/bharathvaj1995/ip-geoinfo) 
+[![Code Climate](https://codeclimate.com/github/codeclimate/codeclimate/badges/gpa.svg)](https://codeclimate.com/github/codeclimate/codeclimate)
+
+# getfilesize   
+
+Get Human Readable file size information.
+
+## Installation
+```
+$ npm install getfilesize --save
+```
+
+## Usuage
+```javascript
+var getFileSize = require('getfilesize');
+console.log(getFileSize('dummy.txt'));
+    /*=> 58.99 KB */
+});
+```
+## API
+
+### getFileSize([filePath])
+Returns string containing the size of the file if exits else returns "File Not Found"
+
+## License
+
+MIT © [Bharathvaj Ganesan](https://github.com/bharathvaj1995)
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let fs = require("fs");
+var fs = require("fs");
+
 function getFileSize(filePath) {
-  var stats = fs.statSync(filePath);
-  console.log('stats', stats);
-  var size = stats["size"];
-  // convert it to humanly readable format.
-  var i = Math.floor( Math.log(size) / Math.log(1024) );
-  return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i];
+
+       if (fs.existsSync(filePath)) {
+
+            // convert it to humanly readable format.
+            var stats = fs.statSync(filePath),                
+                size = stats["size"],
+                i = Math.floor( Math.log(size) / Math.log(1024) );
+
+            return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i];
+        } else {
+            return "File Not Found";
+        }    
 }
-console.log(getFileSize('dummy.txt'));
 module.exports = getFileSize;
