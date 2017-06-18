855c2de9f99a4e4f5fda9f66bcb1f58665415d48
bharath <bharathvaj1995@gmail.com>
2017-06-18 19:06:13 +0530

    Empty file patch

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ function getFileSize(filePath) {
             // convert it to humanly readable format.
             var stats = fs.statSync(filePath),                
                 size = stats["size"],
+                i = 0;
+                if (String(size)==='0') {
+                    return "Empty File";
+                }
                 i = Math.floor( Math.log(size) / Math.log(1024) );
-
             return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i];
         } else {
             return "File Not Found";
